4c4bb2e861a2d4f611ed9d44c8c23504fba64388
VIAlexRus <vialex.92@gmail.com>
2020-01-26 20:43:48 +0300

    26.01.2020

--- Lesson 1/task_1.py
+++ Lesson 1/task_1.py
@@ -1,5 +1,5 @@
 # You are asked to ensure that the first and last names of people begin with
-# a capital letter in their passports. test
+# a capital letter in their passports.
 # For example, alison heck should be capitalized correctly as Alison Heck.
 # Given a full name, your task is to capitalize the name appropriately.
 
